b5268039d78eeb33c9f233b7a0645c8a91ec25d4
kentur <kevinxrturop@gmail.com>
2023-09-29 10:52:25 -0500

    Update i18n.js

--- src/language/i18n.js
+++ src/language/i18n.js
@@ -1,9 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import { english } from './en'
-import { bengali } from './bn'
-import { arabic } from './ar'
-import { vietnam } from './vi'
+import { spain } from './es'
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -12,24 +10,18 @@ const resources = {
     en: {
         translation: english,
     },
-    bn: {
-        translation: bengali,
-    },
-    ar: {
-        translation: arabic,
-    },
-    vi: {
-        translation: vietnam,
+    es: {
+        translation: spain,
     },
 }
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+        lng: 'es', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
         // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
         // if you're using a language detector, do not define the lng option
-        fallbackLng: 'en',
+        fallbackLng: 'es',
         interpolation: {
             escapeValue: false, // react already safes from xss
         },
