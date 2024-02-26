1b975e3ada9f2e3780c0a2182e1e9d5b5a94b459
kevinroses <kevinxrturop@gmail.com>
2024-02-26 16:55:30 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ['stackfood.6am.one'],
+    },
+    staticPageGenerationTimeout: 60, // Aumentar el tiempo de espera a 120 segundos (o ajusta según sea necesario)
+}
